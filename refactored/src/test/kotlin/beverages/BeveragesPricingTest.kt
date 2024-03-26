1d03e77f5321a5f6404670163fee8c72d964a757
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-03-26 08:31:05 +0100

    Refactor on BeveragePricingTest: Replace tests by a parameterized one

--- refactored/src/test/kotlin/beverages/BeveragesPricingTest.kt
+++ refactored/src/test/kotlin/beverages/BeveragesPricingTest.kt
@@ -8,49 +8,23 @@ import beverages.Extra.MILK
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.number.IsCloseTo.closeTo
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
 
 class BeveragesPricingTest {
 
-    @Test
-    fun `should compute coffee price`() {
-        val coffee = Beverage(type = COFFEE)
-        assertThat(coffee.price(), `is`(closeTo(1.20, 0.001)))
-    }
-
-    @Test
-    fun `should compute tea price`() {
-        val tea = Beverage(type = TEA)
-        assertThat(tea.price(), `is`(closeTo(1.50, 0.001)))
-    }
-
-    @Test
-    fun `should compute hot chocolate price`() {
-        val hotChocolate = Beverage(type = HOT_CHOCOLATE)
-        assertThat(hotChocolate.price(), `is`(closeTo(1.45, 0.001)))
-    }
-
-    @Test
-    fun `should compute tea with milk price`() {
-        val teaWithMilk = Beverage(type = TEA, extras = listOf(MILK))
-        assertThat(teaWithMilk.price(), `is`(closeTo(1.60, 0.001)))
-    }
-
-    @Test
-    fun `should compute coffee with milk price`() {
-        val coffeeWithMilk = Beverage(type = COFFEE, extras = listOf(MILK))
-        assertThat(coffeeWithMilk.price(), `is`(closeTo(1.30, 0.001)))
-    }
-
-    @Test
-    fun `should compute coffee with milk and cream price`() {
-        val coffeeWithMilkAndCream = Beverage(type = COFFEE, extras = listOf(MILK, CREAM))
-        assertThat(coffeeWithMilkAndCream.price(), `is`(closeTo(1.45, 0.001)))
-    }
-
-    @Test
-    fun `should compute hot chocolate with cream price`() {
-        val hotChocolateWithCream = Beverage(type = HOT_CHOCOLATE, extras = listOf(CREAM))
-        assertThat(hotChocolateWithCream.price(), `is`(closeTo(1.60, 0.001)))
+    @TestFactory
+    fun `beverage prices`() = listOf(
+        Triple(Beverage(type = COFFEE), 1.20, 0.001),
+        Triple(Beverage(type = TEA), 1.50, 0.001),
+        Triple(Beverage(type = HOT_CHOCOLATE), 1.45, 0.001),
+        Triple(Beverage(type = TEA, extras = listOf(MILK)), 1.60, 0.001),
+        Triple(Beverage(type = COFFEE, extras = listOf(MILK)), 1.30, 0.001),
+        Triple(Beverage(type = COFFEE, extras = listOf(MILK, CREAM)), 1.45, 0.001),
+        Triple(Beverage(type = HOT_CHOCOLATE, extras = listOf(CREAM)), 1.60, 0.001)
+    ).map { (beverage, price, error) ->
+        dynamicTest("should compute ${beverage.type} with extras ${beverage.extras}") {
+            assertThat(beverage.price(), `is`(closeTo(price, error)))
+        }
     }
 }
