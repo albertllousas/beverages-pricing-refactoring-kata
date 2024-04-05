3a4b545ac135cdc6de1edd89f91c7422a87222ad
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-04-05 12:36:23 +0200

    Refactor: extract defaults to an object and imperativize the code for better readability

--- refactored/src/main/kotlin/beverages/Beverage.kt
+++ refactored/src/main/kotlin/beverages/Beverage.kt
@@ -6,28 +6,30 @@ data object Tea : BeverageType
 data object HotChocolate : BeverageType
 
 sealed interface Supplement
-data object Milk: Supplement
-data object Cream: Supplement
-data object Cinnamon: Supplement
-data class Extra(val name: String): Supplement
+data object Milk : Supplement
+data object Cream : Supplement
+data object Cinnamon : Supplement
+data class Extra(val name: String) : Supplement
 
-open class Beverage(
+data class Beverage(
     val type: BeverageType,
     val supplements: List<Supplement> = emptyList(),
-    private val beveragePrices: Map<BeverageType, Double> = DEFAULT_BEVERAGE_PRICES,
-    private val supplementPrices: Map<Supplement, Double> = DEFAULT_SUPPLEMENT_PRICES
+    private val beveragePrices: Map<BeverageType, Double> = BeverageConstants.DEFAULT_BEVERAGE_PRICES,
+    private val supplementPrices: Map<Supplement, Double> = BeverageConstants.DEFAULT_SUPPLEMENT_PRICES
 ) {
 
-    companion object {
-        val DEFAULT_BEVERAGE_PRICES: Map<BeverageType, Double> =
-            mapOf(Coffee to 1.2, Tea to 1.50, HotChocolate to 1.45)
-        val DEFAULT_SUPPLEMENT_PRICES: Map<Supplement, Double> =
-            mapOf(Milk to 0.10, Cream to 0.15, Cinnamon to 0.05)
+    fun price(): Double {
+        val price = beveragePrices[type] ?: throw PriceNotPresentException(this.type.toString())
+        val supplements = supplements.sumOf { supplementPrices[it] ?: throw PriceNotPresentException(it.toString()) }
+        return price + supplements
     }
-
-    open fun price(): Double =
-        (beveragePrices[type] ?: throw PriceNotPresentException(this.type.toString())) +
-                supplements.sumOf { supplementPrices[it] ?: throw PriceNotPresentException(it.toString()) }
 }
 
 data class PriceNotPresentException(val item: String) : Exception("Price for $item is not present")
+
+object BeverageConstants {
+
+    val DEFAULT_BEVERAGE_PRICES: Map<BeverageType, Double> = mapOf(Coffee to 1.2, Tea to 1.50, HotChocolate to 1.45)
+
+    val DEFAULT_SUPPLEMENT_PRICES: Map<Supplement, Double> = mapOf(Milk to 0.10, Cream to 0.15, Cinnamon to 0.05)
+}
